e77123801d021dd98df4e1f706a18e5e44aae3a1
Ksenia <110953599+KseGreb@users.noreply.github.com>
2024-04-08 13:24:39 -0600

    added food photos

---
+++ src/Components/DishesComponents/Dish.js
@@ -0,0 +1,8 @@
+
+export const Dish = () => {
+    return(
+        <div>
+            EveryDish!!!!!!!!!!!!
+        </div>
+    )
+}
--- src/Components/DishesComponents/Dishes.js
+++ src/Components/DishesComponents/Dishes.js
@@ -1,5 +1,11 @@
+import { Dish } from "./Dish"
+import dishesData from "../../data/DataDishes"
+
 export const Dishes = () =>{
     return(<div>
         Dishes!
+    
+        {dishesData.map(dish => <Dish dish={dish}/>)}
+
     </div>)
 }
