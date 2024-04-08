8c60370550bfcc75f37763ae3609fba5ca84b22f
Ksenia <110953599+KseGreb@users.noreply.github.com>
2024-04-08 15:00:04 -0600

    added css

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -6,6 +6,6 @@ export const Filter = ({category}) => {
     const selectedCategory = useSelector(getSelectedCategory);
 
     return(<div>
-        <p className={selectedCategory === category ? "categoryButtonSelected" : "categoryButton"}>{category}</p>
+        <p className={selectedCategory === category ? "categoryButtonSelected categoryButton" : "categoryButton"}>{category}</p>
     </div>)
 } 
