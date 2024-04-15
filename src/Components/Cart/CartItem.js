89ee76840f2639ed668748570a29578be7bd929d
Ksenia <110953599+KseGreb@users.noreply.github.com>
2024-04-15 14:53:37 -0600

    added total price in cartSlice and cart

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -5,8 +5,10 @@ export const CartItem = ({cartItem}) =>{
 
     const dishes = dishesData.find(item => item.id === cartItem.dishId);
     console.log(dishes);
+
     return(<div>
         <p>{dishes.name}</p>
         <p> {cartItem.quantity} portion(s)</p>
+        <p>Price: ${dishes.price * cartItem.quantity}</p>
     </div>)
 }
